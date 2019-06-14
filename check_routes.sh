506292b70bf479acc89a264cbd0e7f6b9529dd98
mma-pivotal <mma@pivotal.io>
2019-06-14 19:26:37 +0900

    added comment into scripts

--- check_routes.sh
+++ check_routes.sh
@@ -10,9 +10,13 @@ export ROUTER_INSTANCE_NUM= <number_of_gorouter>
 
 bosh -d $DEPLOYMENT_NAME ssh diego_cell/0 -c "source /var/vcap/jobs/cfdot/bin/setup; cfdot actual-lrp-groups > /tmp/cfdot.json"
 bosh -d $DEPLOYMENT_NAME scp diego_cell/0:/tmp/cfdot.json /tmp/cfdot.json
+#download the cfdot files
 
 for i in $(seq 1 $ROUTER_INSTANCE_NUM); do
+  #as there could be multiple gorouters and each may have different routing table, we need to check them one by one
   pass=$(bosh -d $DEPLOYMENT_NAME ssh router/0 -c 'head -5 /var/vcap/jobs/gorouter/config/gorouter.yml' | grep pass | awk '{print $5}' | tr -d '\r')
   bosh -d $DEPLOYMENT_NAME ssh router/0 -c "curl router_status:$pass@localhost:8080/routes" | grep stdout | awk '{print $4}' > /tmp/routes.json
+  echo "checking router/$ROUTER_INSTANCE_NUM"
   python compare.py
+  #call py script as it's much easier when doing json parsing
 done
--- compare.py
+++ compare.py
@@ -2,17 +2,21 @@ import json
 import re
 import subprocess
 
-bashCommand= "cf login -a api.run-13.haas-59.pez.pivotal.io -u admin -p M0SkMbsI0cpNJgEX7djis_75fYFdP7mg -o system -s system --skip-ssl-validation"
+#Please replace the credentials here, make sure you can cf login with the correct credentails
+bashCommand= "cf login -a <api_endpoint> -u <admin> -p <pass> -o system -s system --skip-ssl-validation"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
 
-app = {}
+gorouter = {}
 
+#read routes from gorouter routing table
 with open('/tmp/routes.json') as routes:
     data=json.load(routes)
     for host in data.keys():
         for item in data[host]:
             address = item['address']
-            app[address] = host
+            gorouter[address] = host
+            # gorouter example : [{"192.168.1.1:8080","app1.domain.com"},xxx] , key = ip+port, value = route
 
 with open("/tmp/cfdot.json") as cfdot:
     file_data = cfdot.read()
@@ -20,18 +24,32 @@ with open("/tmp/cfdot.json") as cfdot:
     file_data = file_data[:-1]
     file_data = "[" + file_data + "]"
     lrp = json.loads(file_data)
+    #ugly file convertion to load single line json object
 
-cfdot_host={}
+count=0
+#count of stale routes
 
 for key in lrp:
     ip = key["instance"]["address"]
     port = key["instance"]["ports"][0]["host_tls_proxy_port"]
-    app_guid = key["instance"]["process_guid"][:36]
-    print(app_guid)
     address = ip+":"+str(port)
-    if address in app:
-        if address in cfdot_host:
-            if cfdot_host[address] != app[address]:
-                print("Stale Route Found :",address,app[address],cfdot_host[address])
-                continue
-        cfdot_host[address] = app[address]
+    #address comes from LRP
+    app_guid = key["instance"]["process_guid"][:36]
+    bashCommand = "cf curl /v2/gorouters/"+app_guid+"/stats"
+    #cf curl /v2/apps/<guid>/stats, uri here comes from LRP
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    output_string = output.decode("utf-8")
+    app_stats=json.loads(output_string)
+    app_name=app_stats["0"]["stats"]["uris"]
+    #app can have multiple routes, so app_name could be a list
+    if address in gorouter:
+        #if we can find the lrp ip:host in gorouter's routing table
+        if not gorouter[address] in app_name:
+            #if gorouter's routing table doesn't match LRP's ip:host
+            print("Possible Stale Route Found, Gorouter gorouter hostname:",gorouter[address],"\n Diego Cell gorouter hostname:",app_name)
+            #this script doesn't work very well with multiple routes, so please manually check the app even if you see a warning
+            count += 1
+
+if count == 0:
+    print("No Stale Route Found")
