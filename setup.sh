6bf5b67b6dc19db016aa97d392df20dce238df30
mma-pivotal <mma@pivotal.io>
2020-07-15 14:30:54 +0900

    added setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export BOSH_CLIENT= <uaa_bosh_client>
+export BOSH_CLIENT_SECRET= <uaa_bosh_secret>
+export BOSH_CA_CERT= <path_to_ca_cert>
+export BOSH_ENVIRONMENT= <ip_of_bosh_director>
+export APP_NAME= <app_name>
+export DEPLOYMENT_NAME= <dep_name>
+export ROUTER_INSTANCE_NUM= <number_of_gorouter>
+
+export API_ENDPONIT= <api_endpoint>
+export CF_ADMIN= <admin>
+export CF_PASS= <pass>
