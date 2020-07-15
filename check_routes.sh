548c39c22653ccecc57ce0838cba1cb23296d25b
mma-pivotal <mma@pivotal.io>
2020-07-15 15:13:48 +0900

    Updated environment variable in python script

--- check_routes.sh
+++ check_routes.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-export BOSH_CLIENT= <uaa_bosh_client>
-export BOSH_CLIENT_SECRET= <uaa_bosh_secret>
-export BOSH_CA_CERT= <path_to_ca_cert>
-export BOSH_ENVIRONMENT= <ip_of_bosh_director>
-export APP_NAME= <app_name>
-export DEPLOYMENT_NAME= <dep_name>
-export ROUTER_INSTANCE_NUM= <number_of_gorouter>
+source setup.sh
 
-bosh -d $DEPLOYMENT_NAME ssh diego_cell/0 -c "source /var/vcap/jobs/cfdot/bin/setup; cfdot actual-lrp-groups > /tmp/cfdot.json"
+bosh -d $DEPLOYMENT_NAME ssh diego_cell/0 -c "source /var/vcap/jobs/cfdot/bin/setup; cfdot actual-lrps > /tmp/cfdot.json"
 bosh -d $DEPLOYMENT_NAME scp diego_cell/0:/tmp/cfdot.json /tmp/cfdot.json
 #download the cfdot files
 
@@ -16,7 +10,7 @@ for i in $(seq 1 $ROUTER_INSTANCE_NUM); do
   #as there could be multiple gorouters and each may have different routing table, we need to check them one by one
   pass=$(bosh -d $DEPLOYMENT_NAME ssh router/0 -c 'head -5 /var/vcap/jobs/gorouter/config/gorouter.yml' | grep pass | awk '{print $5}' | tr -d '\r')
   bosh -d $DEPLOYMENT_NAME ssh router/0 -c "curl router_status:$pass@localhost:8080/routes" | grep stdout | awk '{print $4}' > /tmp/routes.json
-  echo "checking router/$ROUTER_INSTANCE_NUM"
+  echo "checking router/$i"
   python compare.py
   #call py script as it's much easier when doing json parsing
 done
--- compare.py
+++ compare.py
@@ -1,9 +1,14 @@
 import json
 import re
 import subprocess
+import os
+
+api_endpoint = os.getenv('API_ENDPOINT')
+cf_admin = os.getenv('CF_ADMIN')
+cf_pass = os.getenv('CF_PASS')
 
 #Please replace the credentials here, make sure you can cf login with the correct credentails
-bashCommand= "cf login -a <api_endpoint> -u <admin> -p <pass> -o system -s system --skip-ssl-validation"
+bashCommand= "cf login -a "+api_endpoint+" -u "+cf_admin+" -p "+cf_pass+" -o system -s system --skip-ssl-validation"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 
@@ -28,26 +33,41 @@ with open("/tmp/cfdot.json") as cfdot:
 
 count=0
 #count of stale routes
+stale_apps = []
+#list of stale apps
 
 for key in lrp:
-    ip = key["instance"]["address"]
-    port = key["instance"]["ports"][0]["host_tls_proxy_port"]
+    ip = key["address"]
+    if not ip:
+    #if an application container is crashed, it will not have an IP address
+    #we only care about running containers so skip this crashed one
+        continue
+    port = key["ports"][0]["host_tls_proxy_port"]
     address = ip+":"+str(port)
     #address comes from LRP
-    app_guid = key["instance"]["process_guid"][:36]
-    bashCommand = "cf curl /v2/gorouters/"+app_guid+"/stats"
+    app_guid = key["process_guid"][:36]
+    bashCommand = "cf curl /v2/apps/"+app_guid+"/stats"
     #cf curl /v2/apps/<guid>/stats, uri here comes from LRP
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     output_string = output.decode("utf-8")
-    app_stats=json.loads(output_string)
+    output_string = output_string.lower()
+    if "the app could not be found" in output_string:
+        if app_guid in stale_apps:
+            continue
+        print("Possible stale application found:",app_guid)
+        print("This app is listed as actual LRP but could not be found from Cloud Controller")
+        stale_apps.append(app_guid)
+        continue
+    else:
+        app_stats=json.loads(output_string)
     app_name=app_stats["0"]["stats"]["uris"]
     #app can have multiple routes, so app_name could be a list
     if address in gorouter:
         #if we can find the lrp ip:host in gorouter's routing table
         if not gorouter[address] in app_name:
             #if gorouter's routing table doesn't match LRP's ip:host
-            print("Possible Stale Route Found, Gorouter gorouter hostname:",gorouter[address],"\n Diego Cell gorouter hostname:",app_name)
+            print("Possible Stale Route Found, Gorouter hostname:",gorouter[address],"\n Diego Cell hostname:",app_name)
             #this script doesn't work very well with multiple routes, so please manually check the app even if you see a warning
             count += 1
 
